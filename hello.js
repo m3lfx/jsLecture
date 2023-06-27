03585d2a932d8fe32aa94faaae393ee21c6e2067
rommel dalisay <rommelmdalisay@gmail.com>
2023-06-27 10:46:00 +0800

    functions

--- hello.js
+++ hello.js
@@ -1,5 +1,29 @@
-function add(a,b){
-    return a+b;
+// function add(a,b){
+//     return a+b;
+//    }
+//    c = add(1,2);
+//    console.log(c);
+
+// var add = function (a, b) {
+//   return a + b;
+// };
+// c = add(1, 2);
+// console.log(c);
+
+// function changeCase(val) {
+//     return val.toUpperCase();
+//    }
+//    function demofunc(a, passfunction) {
+//     console.log(passfunction(a));
+//    }
+//    demofunc("smallcase", changeCase);
+
+var looper = function(x){
+    if (x%5===0) {
+    return;
+    }
+    console.log(x)
    }
-   c = add(1,2);
-   console.log(c);
+   for(var i=1;i<10;i++){
+    looper(i);
+   }
