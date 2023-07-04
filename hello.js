547996b1db82b220d2d5c03986d4a59e88b008d4
rommel dalisay <rommelmdalisay@gmail.com>
2023-07-04 11:10:24 +0800

    item get ajax

--- hello.js
+++ hello.js
@@ -1132,3 +1132,32 @@ item.index{{-- item.index --}}
       </div>
   </div>
 </div>
+
+$(document).ready(function() { 
+    $.ajax({
+        type: "GET",
+        url: "/api/item",
+        dataType: 'json',
+        success: function (data) {
+            // console.log(data);
+        $.each(data, function(key, value) {
+          // console.log(value);
+          id = value.item_id;
+          var img = "<img src="+value.img_path +" width='200px', height='200px'/>";
+          var tr = $("<tr>");
+          tr.append($("<td>").html(value.item_id));
+          tr.append($("<td>").html(img));
+          tr.append($("<td>").html(value.description));
+          tr.append($("<td>").html(value.cost_price));
+          tr.append($("<td>").html(value.sell_price));
+          tr.append("<td align='center'><a href='#' data-bs-toggle='modal' data-bs-target='#itemModal' id='editbtn' data-id="+ id + "><i class='fa fa-pencil-square-o' aria-hidden='true' style='font-size:24px' ></a></i></td>");
+          tr.append("<td><a href='#'  class='deletebtn' data-id="+ id + "><i  class='fa fa-trash-o' style='font-size:24px; color:red' ></a></i></td>");
+          $("#ibody").append(tr);
+       });
+      },
+        error: function(){
+          console.log('AJAX load did not work');
+          alert("error");
+      }
+        });
+})
