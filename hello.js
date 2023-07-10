f9e831f73639a0874e9efd3ffd8035fd84f05b1f
rommel dalisay <rommelmdalisay@gmail.com>
2023-07-10 15:53:54 +0800

    ajax delete

--- hello.js
+++ hello.js
@@ -1209,3 +1209,47 @@ $("#itemSubmit").on('click', function(e) {
         }
     });
   });
+
+
+$('#itable tbody').on('click', 'a.deletebtn', function (e) {
+        
+        var id = $(this).data('id');
+        var $row = $(this).closest('tr');
+        console.log(id);
+        // console.log(table);
+        e.preventDefault();
+        bootbox.confirm({
+            message: "do you want to delete this customer",
+            buttons: {
+                confirm: {
+                    label: 'yes',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'no',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                console.log(result);
+                if (result)
+                    $.ajax({
+                        type: "DELETE",
+                        url:`/api/items/${id}`,
+                        headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                        dataType: "json",
+                        success: function (data) {
+                            console.log(data);
+                            $row.fadeOut(4000, function () {
+                                $row.remove()
+                            });
+
+                            bootbox.alert(data.success);
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        }
+                    });
+            }
+        });
+    });
