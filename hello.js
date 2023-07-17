7c84cb0f12d2cdc99d98c4e2c0a2dadd216f32a9
rommel dalisay <rommelmdalisay@gmail.com>
2023-07-17 14:54:13 +0800

    dashboard controller

--- hello.js
+++ hello.js
@@ -1644,3 +1644,48 @@ public function destroy($id)
 		$data = array('success' => 'deleted','code'=>200);
         return response()->json($data);
     }
+dashboardcontroller
+public function titleChart() {
+        $customer = DB::table('customer')->groupBy('title')->orderBy('total')->pluck(DB::raw('count(title) as total'),'title')->all();
+        // dd($customer);
+        $labels = (array_keys($customer));
+        
+        $data= array_values($customer);
+        // dd($customer, $data, $labels);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
+
+    public function salesChart() {
+        
+        $sales = DB::table('item as i')
+                    ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+                    ->join('orderinfo as oi', 'ol.orderinfo_id', '=', 'oi.orderinfo_id')
+                    ->select(DB::raw('monthname(oi.date_placed) as month, sum(ol.quantity * i.sell_price) as total'))
+                    ->groupBy('oi.date_placed')
+                    ->pluck('total','month')
+                    ->all();
+                    
+        // dd($sales);
+        $labels = (array_keys($sales));
+        
+        $data= array_values($sales);
+        // dd($sales, $data, $labels);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
+
+    public function itemsChart() {
+        
+        $items = DB::table('item as i')
+                    ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+                    ->select(DB::raw('i.description as items, sum(ol.quantity) as total'))
+                    ->groupBy('i.description')
+                    ->pluck('total','items')
+                    ->all();
+                    
+        // dd($items);
+        $labels = (array_keys($items));
+        
+        $data= array_values($items);
+        // dd($items, $data, $labels);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
